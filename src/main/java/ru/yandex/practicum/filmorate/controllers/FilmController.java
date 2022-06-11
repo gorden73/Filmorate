a8110ff2b584ce4aa1553c2884d0a04cff0d6942
Andrey <dev.andr@yandex.com>
2022-06-11 11:00:16 +0500

    feat: Добавить маппинг getFilmsByDirector в контроллер фильмов

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -61,4 +61,22 @@ public class FilmController {
     public Collection<Film> getRecommendations(@PathVariable Integer id) {
         return filmService.getRecommendations(id);
     }
+
+    @GetMapping("/director/{directorId}")
+    public Collection<Film> getFilmsByDirector(@PathVariable Integer directorId,
+                                               @RequestParam(defaultValue = "year") String sortBy,
+                                               @RequestParam(defaultValue = "10") Integer count,
+                                               @RequestParam(defaultValue = "0") Integer page) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("count");
+        }
+        if (page < 0) {
+            throw new IllegalArgumentException("page");
+        }
+        if (!(sortBy.equals("year") || sortBy.equals("likes"))) {
+            throw new IllegalArgumentException("sortBy");
+        }
+        Integer from = count * page;
+        return filmService.getFilmsByDirector(directorId, sortBy, from, count);
+    }
 }
