4571dcf99373c1e3b7e34f1ab9f48792729a491b
Andrey <dev.andr@yandex.com>
2022-06-11 21:40:52 +0500

    feat: Добавить метод getAllDirectorsById в интерфейс DirectorDao

--- src/main/java/ru/yandex/practicum/filmorate/dao/DirectorDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/DirectorDao.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.dao;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface DirectorDao {
@@ -10,6 +11,8 @@ public interface DirectorDao {
     Optional<Director> create(Director entity);
 
     Collection<Director> getAllDirectors();
+
+    List<Director> getAllDirectorsById(Integer filmId);
 
     Optional<Director> getDirector(Integer id);
 
