dda145f6f6499a510dab935a50616828737d93dd
Andrey <dev.andr@yandex.com>
2022-06-11 13:27:08 +0500

    feat: Добавить тестовых данных

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -18,3 +18,13 @@ VALUES('mail', 'login', 'name', '1990-03-08');
 
 INSERT INTO films(name, description, release_date, duration, mpa)
 VALUES ('The Rock', 'Starring Nicolas Cage and Sean Connery', '1996-06-07', '136', '1');
+
+INSERT INTO directors(name)
+VALUES ('Jon Scott');
+
+INSERT INTO directors(name)
+VALUES ('Jack Tomson');
+
+INSERT INTO FILM_DIRECTOR(director_id, film_id) VALUES ( '1', '3' );
+INSERT INTO FILM_DIRECTOR(director_id, film_id) VALUES ( '1', '2' );
+INSERT INTO FILM_DIRECTOR(director_id, film_id) VALUES ( '2', '3' );
