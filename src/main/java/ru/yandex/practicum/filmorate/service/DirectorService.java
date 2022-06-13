bdb8db9dbc39b73a4c906f4bcbfd9a7f46d8b860
Andrey <dev.andr@yandex.com>
2022-06-13 11:19:03 +0500

    feat: add logging to getAllDirectors

--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -21,24 +21,26 @@ public class DirectorService {
     }
 
     public Optional<Director> createDirector(Director director) {
-        log.info(String.format("Добавлен директор %s", director));
+        log.info(String.format("Добавлен режиссер %s", director));
         return storage.create(director);
     }
 
     public Optional<Director> updateDirector(Director director) {
         findDirectorById(director.getId());
-        log.info(String.format("Обновлен директор %s", director));
+        log.info(String.format("Обновлен режиссер %s", director));
         return storage.update(director);
     }
 
     public Collection<Director> getAllDirectors() {
-        return storage.getAllDirectors();
+        Collection<Director> directorList = storage.getAllDirectors();
+        log.info(String.format("Количество режиссеров %s", directorList.size()));
+        return directorList;
     }
 
     public Optional<Director> findDirectorById(Integer id) {
         final Optional<Director> optionalDirector = storage.getDirector(id);
         if (optionalDirector.isEmpty()) {
-            throw new ElementNotFoundException(String.format("Директор c ID %s не найден", id));
+            throw new ElementNotFoundException(String.format("Режиссер c ID %s не найден", id));
         }
         return storage.getDirector(id);
     }
