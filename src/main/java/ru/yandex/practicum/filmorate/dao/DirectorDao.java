4bcd10e8cb097892e6b0db414b01265eb184e070
Andrey <dev.andr@yandex.com>
2022-06-13 23:04:03 +0500

    add: DirectorDao interface

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/DirectorDao.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.model.Director;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DirectorDao {
+
+    Optional<Director> create(Director entity);
+
+    Collection<Director> getAllDirectors();
+
+    Collection<Director> getAllDirectorsById(Integer filmId);
+
+    Optional<Director> getDirector(Integer id);
+
+    Integer addFilmDirector(Integer directorId, Integer filmId);
+
+    Optional<Director> update(Director entity);
+
+    Integer delete(Integer id);
+}
